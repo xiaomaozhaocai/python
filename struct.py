11521b23b51035f940be3345f84d3e7827c2ca3f
zuopan <linyizuopan@hotmail.com>
2021-12-02 21:30:58 +0800

    something modified

--- struct.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-
-import os
-
-print('欢迎学习python课程')
-print('今天的天气非常好')
-
-print(os.getcwd())
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print('test');
+print(10_000_000 + 1)
+
+print(12e-5 + 1)
+
+print(None)
